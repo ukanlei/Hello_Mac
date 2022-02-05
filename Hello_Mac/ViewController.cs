d386420b3ea0a8f5b4bf46110b9304d1545178f7
ukalelei <uvioletl@hotmail.com>
2022-02-05 14:14:09 -0600

    change label's value when button is clicked
    1. private variable to store number of clicks
    2. Set initial value of label after user interface is loaded and instantiated by using ViewDidLoad() instead of Initialize
    3. Add partial method that responds to user's clicking the button

--- Hello_Mac/ViewController.cs
+++ Hello_Mac/ViewController.cs
@@ -7,15 +7,10 @@ namespace Hello_Mac
 {
     public partial class ViewController : NSViewController
     {
+        private int numberOfTimesClicked = 0;
+
         public ViewController(IntPtr handle) : base(handle)
         {
-        }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-
-            // Do any additional setup after loading the view.
         }
 
         public override NSObject RepresentedObject
@@ -30,5 +25,21 @@ namespace Hello_Mac
                 // Update the view, if already loaded.
             }
         }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+            // Set the initial value for the label
+            ClickedLabel.StringValue = "Button has not been clicked yet.";
+        }
+
+        // method is called any time the user clicks the button
+        partial void ClickedButton(NSObject sender)
+        {
+            ClickedLabel.StringValue = string.Format("The button has been clicked {0} time{1}.", ++numberOfTimesClicked, (numberOfTimesClicked < 2) ? "" : "s");
+        }
+
     }
 }
